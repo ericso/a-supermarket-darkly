8716e0195007e2a1c5b022e0c4156a7b6288b8f4
Eric So <me@eric.so>
2025-07-21 20:45:47 -0700

    use TileMapLayer navigation layer

--- scenes/shelf_menu/ShelfMenu.gd
+++ scenes/shelf_menu/ShelfMenu.gd
@@ -8,9 +8,6 @@ func _ready():
 		btn.text = item
 		btn.pressed.connect(_on_item_selected.bind(item))
 		$VBoxContainer.add_child(btn)
-		
-	for button in $VBoxContainer.get_children():
-		button.pressed.connect(_on_item_selected.bind(button.text))
 
 func _on_item_selected(id: String):
 	if shelf:
