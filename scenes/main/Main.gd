2d4a349fa0f70aa61c60e9a26d3390749fe8f3a0
Eric So <me@eric.so>
2025-07-23 16:39:05 -0700

    small tweaks

--- scenes/main/Main.gd
+++ scenes/main/Main.gd
@@ -4,11 +4,11 @@ const TILE_SIZE = 32
 
 var customer_scene := preload("res://scenes/customer/Customer.tscn")
 
-@onready var store: TileMapLayer = $Store
+@onready var store_map: TileMapLayer = $Store
 
 func _ready():
 	# spawn shelves and customers
-	spawn_customer_at_tile(10, 10)
+	spawn_customer_at_tile(9, 18)
 	spawn_interactables()
 
 func spawn_customer_at_tile(x_tile: int, y_tile: int):
@@ -22,17 +22,17 @@ func spawn_customer_at_tile(x_tile: int, y_tile: int):
 
 # spawn_interactables instantiates all "interactable" tiles
 func spawn_interactables():
-	if store == null:
-		push_error("Store node not found")
+	if store_map == null:
+		push_error("store tilemaplayer node not found")
 	
-	for cell in store.get_used_cells():
-		var tile_data = store.get_cell_tile_data(cell)
+	for cell in store_map.get_used_cells():
+		var tile_data = store_map.get_cell_tile_data(cell)
 		if tile_data and tile_data.get_custom_data("interactable"):
 			var scene_path := tile_data.get_custom_data("scene_path") as String
 			if scene_path != "":
 				var packed_scene := load(scene_path)
 				if packed_scene:
 					var node = packed_scene.instantiate()
-					node.position = store.map_to_local(cell)
+					node.position = store_map.map_to_local(cell)
 					add_child(node)
 					node.name = tile_data.get_custom_data("name")
