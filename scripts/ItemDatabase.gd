378f8778c8eb53b5bedb2cc1927398950e87236c
Eric So <me@eric.so>
2025-07-23 17:55:27 -0700

    add two more items: fish and steak

--- scripts/ItemDatabase.gd
+++ scripts/ItemDatabase.gd
@@ -13,6 +13,18 @@ var items = {
 		"price": 3.95,
 		"texture": preload("res://assets/sprites/items/cereal.png")
 	},
+	"fish": {
+		"id": "fish",
+		"name": "Fish",
+		"price": 7.88,
+		"texture": preload("res://assets/sprites/items/fish.png")
+	},
+	"steak": {
+		"id": "steak",
+		"name": "Steak",
+		"price": 19.01,
+		"texture": preload("res://assets/sprites/items/steak.png")
+	},
 }
 
 func get_item_data(id: String) -> Dictionary:
@@ -20,3 +32,6 @@ func get_item_data(id: String) -> Dictionary:
 
 func get_item_ids() -> Array:
 	return items.keys()
+
+func get_item_count() -> int:
+	return get_item_ids().size()
